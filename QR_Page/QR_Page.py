d3ebfb3d6797ec15d63acf8bd578d3195ed660b8
silvercong <bin091003@naver.com>
2024-08-20 23:23:04 +0900

    📝 ::socket 연결 유지

--- QR_Page/QR_Page.py
+++ QR_Page/QR_Page.py
@@ -5,29 +5,28 @@ from urllib.parse import urlparse, parse_qs
 import sys, qrcode, requests, asyncio, websockets
 from PyQt5.QtWidgets import QApplication, QMainWindow
 from PyQt5.QtGui import QPixmap, QPainter, QBrush, QPainterPath
+from qasync import QEventLoop
 
-# url parsing
 def get_query_string(url):
     response_url = urlparse(url)
     query_string = response_url.query
     parse_string = parse_qs(query_string)
-
     return parse_string
 
 # qr
 url = {base_url/exercise/qr}
 response = requests.get(url)
 qr = qrcode.QRCode(
-    version = 1,
-    error_correction = qrcode.constants.ERROR_CORRECT_L,
-    box_size = 8,
-    border = 4,
+    version=1,
+    error_correction=qrcode.constants.ERROR_CORRECT_L,
+    box_size=8,
+    border=4,
 )
 qr.add_data(response.text)
-qr.make(fit = True)
+qr.make(fit=True)
 
 img = qr.make_image(fill_color="black", back_color="white")
-img.save({png})
+img.save({/QR.png})
 
 # sessionId
 parsingData = get_query_string(response.text)
@@ -58,7 +57,7 @@ async def connect():
                 break
 
 
-def round_image(self, pixmap, radius):
+def round_image(pixmap, radius):
     size = pixmap.size()
 
     rounded = QPixmap(size)
@@ -91,12 +90,18 @@ class QR_Page(QMainWindow):
             "border-radius: 30px;")
 
         pixmap = QPixmap("QR.png").scaled(370, 370)
-        rounded_pixmap = round_image(self, pixmap, 40)
+        rounded_pixmap = round_image(pixmap, 40)
         self.imageLabel.setPixmap(rounded_pixmap)
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
+
+    loop = QEventLoop(app)
+    asyncio.set_event_loop(loop)
+
     mainWindow = QR_Page()
     mainWindow.show()
-    asyncio.get_event_loop().run_until_complete(connect())
-    sys.exit(app.exec_())
+
+    with loop:
+        loop.run_until_complete(connect())
+        sys.exit(app.exec_())
